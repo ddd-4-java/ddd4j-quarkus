7e8dbe646e3a8690fdc50c1ed516ea987a8ebe65
wandl-6A72h <hiwepy@gmail.com>
2026-07-14 23:33:33 +0800

    feat(qrcode): 添加二维码模板功能支持
    - 引入 QrCodeTemplateRegistry 和 QrCodeTemplateBinder 接口及实现
    - 添加 InMemoryQrCodeTemplateRegistry 内存模板注册表
    - 注册单例模式的模板注册表和服务绑定器
    - 更新测试依赖版本以保持模块对齐
    - 添加 JUnit 4.

--- ddd4j-quarkus-extensions/ddd4j-quarkus-extension-qrcode/src/main/java/io/ddd4j/quarkus/qrcode/QrCodeProducer.java
+++ ddd4j-quarkus-extensions/ddd4j-quarkus-extension-qrcode/src/main/java/io/ddd4j/quarkus/qrcode/QrCodeProducer.java
@@ -2,6 +2,9 @@ package io.ddd4j.quarkus.qrcode;
 
 import io.ddd4j.extension.qrcode.DefaultQrCodeService;
 import io.ddd4j.extension.qrcode.QrCodeService;
+import io.ddd4j.extension.qrcode.template.InMemoryQrCodeTemplateRegistry;
+import io.ddd4j.extension.qrcode.template.QrCodeTemplateBinder;
+import io.ddd4j.extension.qrcode.template.QrCodeTemplateRegistry;
 import io.github.hiwepy.zxing.QrCodes;
 import io.quarkus.arc.properties.IfBuildProperty;
 import jakarta.enterprise.context.ApplicationScoped;
@@ -19,6 +22,18 @@ public class QrCodeProducer {
     public QrCodeService qrCodeService(QrCodeConfig config) {
         return new DefaultQrCodeService(QrCodes.encoder(), QrCodes.decoder(),
                 config.concurrency(), config.maxBatchSize());
+    }
+
+    @Produces
+    @Singleton
+    public QrCodeTemplateRegistry qrCodeTemplateRegistry() {
+        return new InMemoryQrCodeTemplateRegistry();
+    }
+
+    @Produces
+    @Singleton
+    public QrCodeTemplateBinder qrCodeTemplateBinder() {
+        return new QrCodeTemplateBinder();
     }
 
     public void close(@Disposes QrCodeService service) throws Exception {
